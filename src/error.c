d6e7fe71fbe1f892640f0526f299a617b6909bac
bert <bert.timmerman@xs4all.nl>
2017-09-11 21:52:17 +0200

    src/error: editing doxygen comments.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/error.c
+++ src/error.c
@@ -1,19 +1,24 @@
-/* TABLIX, PGA general timetable solver                              */
-/* Copyright (C) 2002-2006 Tomaz Solc                                      */
-
-/* This program is free software; you can redistribute it and/or modify    */
-/* it under the terms of the GNU General Public License as published by    */
-/* the Free Software Foundation; either version 2 of the License, or       */
-/* (at your option) any later version.                                     */
-
-/* This program is distributed in the hope that it will be useful,         */
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
-/* GNU General Public License for more details.                            */
-
-/* You should have received a copy of the GNU General Public License       */
-/* along with this program; if not, write to the Free Software             */
-/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
+/**
+ * @file src/error.c
+ *
+ * @brief TABLIX, PGA general timetable solver.
+ *
+ * @author Copyright (C) 2002-2006 Tomaz Solc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
 /* $Id: error.c,v 1.2 2006-09-22 15:46:27 avian Exp $ */
 
